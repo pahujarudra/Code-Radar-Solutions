921f65ab32251ccc4bbe1f8c365f246ba8777fc0
pahujarudra <pahujarudra@gmail.com>
2025-03-28 23:28:37 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -18,6 +18,6 @@ void selectionSort(char arr[][100], int n) {
 
 void printArray(char arr[][100], int n) {
     for(int i = 0; i < n; i++) {
-        printf("%s ", arr[i]);
+        printf("%s\n", arr[i]);
     }
 }
