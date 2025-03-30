60d3991bb21411074cef77ecf26160556e1754a9
pahujarudra <pahujarudra@gmail.com>
2025-03-30 14:58:16 +0530

    Solution to Find the Largest Even Number in an Array

--- Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
+++ Find the Largest Even Number in an Array@@676c02002dbc8c3e7a280b8b/code.c
@@ -1,1 +1,30 @@
 // Your code here...
+#include <stdio.h>
+
+int evenmax(int arr[], int n) {
+    int max = -1000;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] % 2 == 0) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+    }
+    return max;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        scanf("%d ", &arr[i]);
+    }
+    if(evenmax(arr, n) == -1000) {
+        printf("-1");
+    }
+    else {
+        printf("%d", evenmax(arr, n));
+    }
+    return 0;
+}
